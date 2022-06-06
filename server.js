7440146946744d5c5c81086adebaba63ea634f1f
YangTuanAnh <yangtuananh2003@gmail.com>
2022-06-06 09:42:09 +0700

    Add Heroku link

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.get('/api/:rapperName', (req, res) =>
 
 })
 
-app.listen(PORT, () =>
+app.listen(process.env.PORT || PORT, () =>
 {
     console.log(`The server is running on port ${PORT}! You better go catch it!`)
 })
